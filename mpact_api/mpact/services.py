6acc86a0ceb81ad6414696d3f79f38bfb253aa35
hussain <mahmed.hussain521@gmail.com>
2020-11-05 21:28:10 +0530

    added constants

--- mpact_api/mpact/services.py
+++ mpact_api/mpact/services.py
@@ -1,18 +1,17 @@
-import os
-
 from telegram.ext import Updater
 
+from .constants import Constants
 from .models import UserChat, UserData
 from .serializers import ChatDataSerializer, UserDataSerializer
 
-updater = Updater(os.environ.get("TOKEN"), use_context=True)
+updater = Updater(Constants.BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 
 def set_webhook():
     # TODO: we can move this to config file or keep this as a seperate script.
     webhook = updater.bot.setWebhook(
-        "https://8a7e5dd2d909.ngrok.io/listen_msg", allowed_updates=["message"]
+        Constants.WEBHOOK_URL, allowed_updates=[Constants.MESSAGE]
     )
     return webhook  # returns True or False
 
@@ -41,7 +40,7 @@ def save_userdata(userdata):
 
 def save_userchat(chat_data, user_data_inst):
     user_chat_record = UserChat.objects.create(
-        chat_id=chat_data.get("id"), user_id=user_data_inst
+        chat_id=chat_data.get(Constants.ID), user_id=user_data_inst
     )
     user_chat_record.save()
     return True
--- mpact_api/mpact/views.py
+++ mpact_api/mpact/views.py
@@ -1,8 +1,7 @@
-import os
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .constants import Constants
 from .models import ChatData, UserChat, UserData
 from .serializers import UserDataSerializer
 from .services import save_chatdata, save_userchat, save_userdata, set_webhook
@@ -27,16 +26,17 @@ class ListenMessages(APIView):
     """
 
     def post(self, request):
-        message = request.data["message"]
-        chat_data = message.get("chat")
-        from_data = message.get("from")
+        print(Constants.MESSAGE)
+        message = request.data.get(Constants.MESSAGE)
+        chat_data = message.get(Constants.CHAT)
+        from_data = message.get(Constants.FROM)
 
-        if "new_chat_participant" in message:
+        if Constants.NEW_CHAT_PARTICIPANT in message:
             # This is used whenever bot or some other members are added to the group
-            user_data = message.get("new_chat_participant")
-            username = user_data.get("username")
+            user_data = message.get(Constants.NEW_CHAT_PARTICIPANT)
+            username = user_data.get(Constants.USERNAME)
 
-            if username == os.environ.get("BOT_USERNAME"):
+            if username == Constants.BOT_USERNAME:
                 # when our bot is added to the group. Bot will be saved to the Chat Data Model
                 return Response(save_chatdata(chat_data))
             else:
@@ -46,18 +46,19 @@ class ListenMessages(APIView):
                     # Mapping between user and group is saved in UserChat Model
                     return Response(save_userchat(chat_data, user_data_inst))
 
-        elif "new_chat_title" in message:
+        elif Constants.NEW_CHAT_TITLE in message:
             # This is used whenever group title is changed.
-            chat_instance = ChatData.objects.get(pk=chat_data.get("id"))
+            chat_instance = ChatData.objects.get(pk=chat_data.get(Constants.ID))
             return Response(save_chatdata(chat_data, chat_instance))
 
-        elif "text" in message:
+        elif Constants.TEXT in message:
             # This is used whenever someone messages in the group.
             # It is used to store the existing user's details to UserData and UserChat Models
             try:
-                user_instance = UserData.objects.get(pk=from_data.get("id"))
+                user_instance = UserData.objects.get(pk=from_data.get(Constants.ID))
                 user_chat_instance = UserChat.objects.get(
-                    chat_id=chat_data.get("id"), user_id__id=from_data.get("id")
+                    chat_id=chat_data.get(Constants.ID),
+                    user_id__id=from_data.get(Constants.ID),
                 )
             except (UserData.DoesNotExist, UserChat.DoesNotExist):
                 user_serializer = UserDataSerializer(data=from_data)
@@ -66,7 +67,7 @@ class ListenMessages(APIView):
                     user_data_rec = UserData.objects.create(**from_data)
                     user_data_rec.save()
                     return Response(save_userchat(chat_data, user_data_rec))
-                elif user_serializer.errors.get("id"):
+                elif user_serializer.errors.get(Constants.ID):
                     # User already exists in user data model but does not exist in userchat model
                     return Response(save_userchat(chat_data, user_instance))
 
@@ -74,5 +75,5 @@ class ListenMessages(APIView):
             user_serializer = UserDataSerializer(user_instance, data=from_data)
             if user_serializer.is_valid():
                 user_serializer.save()
-                return Response("ok")
-        return Response("ok")
+                return Response(Constants.OK)
+        return Response(Constants.OK)
