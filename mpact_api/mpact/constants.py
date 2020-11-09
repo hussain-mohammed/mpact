5ac172e01939c831bb6f5ca408ff19785c6fba8f
hussain <mahmed.hussain521@gmail.com>
2020-11-09 15:41:16 +0530

    removed constants from Constants class

--- mpact_api/mpact/constants.py
+++ mpact_api/mpact/constants.py
@@ -1,26 +1,28 @@
 import os
 
 
-class Constants:
-    ID = "id"
-    MESSAGE = "message"
-    CHAT = "chat"
-    FROM = "from"
-    TEXT = "text"
-    OK = "ok"
-    NEW_CHAT_PARTICIPANT = "new_chat_participant"
-    USERNAME = "username"
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    NEW_CHAT_TITLE = "new_chat_title"
-    BOT_TOKEN = os.environ.get("TOKEN")
-    WEBHOOK_URL = "https://f784fbaa700a.ngrok.io/listen_msg"
-
-
-WEBHOOK_END = "Webhook call end"
-CREATE_UPDATE_MSG = "%s the %s data record: %s"
-INVALID_MSG = "Could not save the %s data, Invalid data"
-DATA_SAVED = "%s data saved"
+ID = "id"
+MESSAGE = "message"
+CHAT = "chat"
+FROM = "from"
+TEXT = "text"
+OK = "ok"
+NEW_CHAT_PARTICIPANT = "new_chat_participant"
+USERNAME = "username"
+NEW_CHAT_TITLE = "new_chat_title"
 USER = "user"
 USER_CHAT = "User chat"
 UPADTING = "Updating"
 CREATING = "Creating"
+WEBHOOK_URL = "https://f784fbaa700a.ngrok.io/listen_msg"
+WEBHOOK_END = "Webhook call end"
+
+
+# Response Messages
+CREATE_UPDATE_MSG = "%s the %s data record: %s"
+INVALID_MSG = "Could not save the %s data, Invalid data"
+DATA_SAVED = "%s data saved"
+
+# Environment Variables
+BOT_TOKEN = os.environ["TOKEN"]
+BOT_USERNAME = os.environ["BOT_USERNAME"]
--- mpact_api/mpact/services.py
+++ mpact_api/mpact/services.py
@@ -1,19 +1,18 @@
 from telegram.ext import Updater
 
 from . import constants
-from .constants import Constants
 from .logger import logger
 from .models import UserChat, UserData
 from .serializers import ChatDataSerializer, UserDataSerializer
 
-updater = Updater(Constants.BOT_TOKEN, use_context=True)
+updater = Updater(constants.BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 
 def set_webhook():
     # TODO: we can move this to config file or keep this as a seperate script.
     webhook = updater.bot.setWebhook(
-        Constants.WEBHOOK_URL, allowed_updates=[Constants.MESSAGE]
+        constants.WEBHOOK_URL, allowed_updates=[constants.MESSAGE]
     )
     return webhook  # returns True or False
 
@@ -23,20 +22,20 @@ def save_chatdata(chat_data, chat_instance=None):
     if chat_instance:
         # To update already existing record
         logger.debug(
-            constants.CREATE_UPDATE_MSG, constants.UPADTING, Constants.CHAT, chat_data
+            constants.CREATE_UPDATE_MSG, constants.UPADTING, constants.CHAT, chat_data
         )
         chat_serializer = ChatDataSerializer(chat_instance, data=chat_data)
     else:
         # To create a new record
         logger.debug(
-            constants.CREATE_UPDATE_MSG, constants.CREATING, Constants.CHAT, chat_data
+            constants.CREATE_UPDATE_MSG, constants.CREATING, constants.CHAT, chat_data
         )
         chat_serializer = ChatDataSerializer(data=chat_data)
     if chat_serializer.is_valid():
         chat_serializer.save()
-        logger.debug(constants.DATA_SAVED, Constants.CHAT)
+        logger.debug(constants.DATA_SAVED, constants.CHAT)
         return True
-    logger.debug(constants.INVALID_MSG, Constants.CHAT)
+    logger.debug(constants.INVALID_MSG, constants.CHAT)
     return False
 
 
@@ -56,7 +55,7 @@ def save_userdata(userdata):
 
 def save_userchat(chat_data, user_data_inst):
     user_chat_record = UserChat.objects.create(
-        chat_id=chat_data.get(Constants.ID), user_id=user_data_inst
+        chat_id=chat_data.get(constants.ID), user_id=user_data_inst
     )
     user_chat_record.save()
     logger.debug(constants.DATA_SAVED, constants.USER_CHAT)
