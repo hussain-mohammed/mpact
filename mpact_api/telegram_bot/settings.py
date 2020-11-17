578164bf27703172f5adce58f5217fb909530e35
Norman Hooper <nhooper@dimagi.com>
2020-11-18 01:14:57 +0200

    DEBUG defaults to False

--- mpact_api/telegram_bot/settings.py
+++ mpact_api/telegram_bot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+if os.environ.get('DEPLOY_ENV') == 'dev':
+    DEBUG = True
