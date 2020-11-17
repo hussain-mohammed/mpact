5adaddd4de8060a3506d5eaaf15062c8ed8b2346
Norman Hooper <nhooper@dimagi.com>
2020-11-18 01:21:41 +0200

    Use DATABASE_URL env var to configure database

--- mpact_api/telegram_bot/settings.py
+++ mpact_api/telegram_bot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,21 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "telegram_bot.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mpact",
-        "USER": "admin",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -127,3 +113,11 @@ STATIC_URL = "/static/"
 
 if os.environ.get('DEPLOY_ENV') == 'dev':
     DEBUG = True
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+
+else:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
