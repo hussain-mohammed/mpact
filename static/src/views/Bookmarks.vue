b43713c8c6f1ab893bdab1d20e89014b931ff330
hussain <mahmed.hussain521@gmail.com>
2021-02-03 16:05:22 +0530

    update flagged-messages page UI

--- static/src/views/Bookmarks.vue
+++ static/src/views/Bookmarks.vue
@@ -4,8 +4,10 @@
       <div class='col-10 p-0'>
         <chat-window height='100vh' class='bookmarks-widget-1' :currentUserId='currentUserId' :messages='bookmarks'
           :single-room='true' :rooms-loaded='true' :rooms='rooms' :text-messages="textMessages"
-          :show-footer='false' :messages-loaded='bookmarksLoaded' @fetch-messages='fetchMoreBookmarks($event)'
-          :message-actions='messageActions' @message-action-handler='messageActionHandler($event)'/>
+          :show-footer='false' :show-emojis='false' :show-reaction-emojis='false' :messages-loaded='bookmarksLoaded'
+          @fetch-messages='fetchMoreBookmarks($event)' :message-actions='messageActions'
+          :styles='styles' :show-new-messages-divider='false'
+          @message-action-handler='messageActionHandler($event)'/>
       </div>
     </div>
   </div>
@@ -38,9 +40,12 @@ export default {
         },
       ],
       textMessages: {
-        ROOMS_EMPTY: 'No flagged message to show',
-        ROOM_EMPTY: 'No flagged message to show',
-        MESSAGES_EMPTY: 'No flagged message to show',
+        ROOMS_EMPTY: 'No flagged messages to show',
+        ROOM_EMPTY: 'No flagged messages to show',
+        MESSAGES_EMPTY: 'No flagged messages to show',
+        CONVERSATION_STARTED: '',
+      },
+      styles: {
       },
     };
   },
@@ -69,45 +74,83 @@ export default {
     async fetchBookmarks() {
       try {
         const result = await MessageService.fetchFlaggedMessages({});
-        this.currentUserId = 1512271983;
-        const formattedMessages = [];
-        result.data.flagged_message.forEach((d) => {
-          formattedMessages.push({
-            _id: d.id,
-            content: d.message,
-            sender_id: d.sender || '',
-            date: convertDate(d.date),
-            timestamp: convertTime(d.date),
+        if (result && result.data.flagged_messages) {
+          this.currentUserId = 1512271983;
+          const formattedMessages = [];
+          result.data.flagged_messages.forEach((d) => {
+            formattedMessages.push({
+              _id: d.id,
+              content: d.message || '',
+              sender_id: d.sender || 1,
+              date: convertDate(d.date),
+              timestamp: convertTime(d.date),
+            });
           });
-        });
-        this.bookmarks = formattedMessages;
-        this.rooms = [{
-          roomId: 1,
-          roomName: 'Bookmarks',
-          users: [],
-        }];
-        this.bookmarksLoaded = true;
+          this.bookmarks = formattedMessages;
+          this.rooms = [{
+            roomId: 1,
+            roomName: 'Bookmarks',
+            users: [],
+          }];
+          if (formattedMessages.length < 50) {
+            this.bookmarksLoaded = true;
+          }
+        }
       } catch (err) {
         console.error(err);
       }
     },
-    async fetchMoreBookmarks() {
+    async fetchMoreBookmarks({
+      options = {},
+    }) {
       try {
+        const {
+          reset = false,
+        } = options;
+        if (reset) {
+          return;
+        }
         const params = {
           offset: this.offset,
           limit: this.limit,
         };
-        await MessageService.fetchFlaggedMessages(params);
+        const result = await MessageService.fetchFlaggedMessages(params);
+        if (result && result.data.flagged_messages) {
+          const formattedMessages = [];
+          result.data.flagged_messages.forEach((d) => {
+            formattedMessages.push({
+              _id: d.id,
+              content: d.message || '',
+              sender_id: d.sender || 1,
+              date: convertDate(d.date),
+              timestamp: convertTime(d.date),
+            });
+          });
+          this.bookmarks = [formattedMessages, ...this.bookmarks];
+          this.rooms = [{
+            roomId: 1,
+            roomName: 'Bookmarks',
+            users: [],
+          }];
+          if (formattedMessages.length < 50) {
+            this.bookmarksLoaded = true;
+          }
+        }
       } catch (err) {
         console.error(err);
       }
     },
     async unFlagMessage({ message }) {
       try {
+        const id = message._id;
         const params = {
-          id: message._id,
+          id,
         };
-        await MessageService.deleteFlaggedMessage(params);
+        const response = await MessageService.unFlagMessage(params);
+        if (response && response.data.is_success) {
+          const updatedBookmarks = this.bookmarks.filter((bookmark) => bookmark._id !== id);
+          this.bookmarks = updatedBookmarks;
+        }
       } catch (err) {
         console.error(err);
       }
@@ -116,4 +159,7 @@ export default {
 };
 </script>
 <style scoped>
+.row {
+  justify-content: center;
+}
 </style>
