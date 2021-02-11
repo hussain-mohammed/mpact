08781855b9dc469562f75db3649f3945483418f4
hussain <mahmed.hussain521@gmail.com>
2021-02-11 13:00:45 +0530

    add prompt to confirm flag deletion

--- static/src/components/Toast.vue
+++ static/src/components/Toast.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="['toast', 'position-absolute', 'mx-auto', { 'bg-danger text-white': hasError }]" aria-live='assertive'
     aria-atomic='true' role='alert' data-delay='2000'>
-    <div class='toast-body'>{{ text }}</div>
+    <div class='toast-body text-center'>{{ text }}</div>
   </div>
 </template>
 
